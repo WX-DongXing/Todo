b5831c4f17d08cca5fb7aaef950be1d8f8966584
xd <xingdong.com@live.com>
2018-12-10 13:37:58 +0800

    Completed！

--- src/app/introduce/intro-ng/intro-ng.component.ts
+++ src/app/introduce/intro-ng/intro-ng.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroNgComponent implements OnInit {
 
+  text: '';
   constructor() { }
 
   ngOnInit() {
--- src/app/introduce/introduce.module.ts
+++ src/app/introduce/introduce.module.ts
@@ -4,23 +4,19 @@ import { IntroduceComponent } from './introduce.component';
 import {IntroduceRouting} from './introduce.routing';
 import {MarkdownModule} from 'ngx-markdown';
 import { IntroNavComponent } from './intro-nav/intro-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { IntroTsComponent } from './intro-ts/intro-ts.component';
 import { IntroNgComponent } from './intro-ng/intro-ng.component';
+import {ShareModule} from '../share/share.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [IntroduceComponent, IntroNavComponent, IntroTsComponent, IntroNgComponent],
   imports: [
     CommonModule,
+    FormsModule,
     IntroduceRouting,
+    ShareModule,
     MarkdownModule.forChild(),
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
   ]
 })
 export class IntroduceModule { }
